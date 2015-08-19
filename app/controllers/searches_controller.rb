80b2662deb9267660c8837332b6ac7216b875d47
carlos <carlosh10@gmail.com>
2015-08-19 15:19:47 -0300

    implemented basic elasticsearch serch

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -2,6 +2,7 @@ class SearchesController < ApplicationController
   def new
   end
   def create
+    #TODO: Associate Searches with a user
     client = Elasticsearch::Client.new host: 'http://104.197.50.109:9400';
 
     search = Search.new(params[:search].permit!)
@@ -13,7 +14,7 @@ class SearchesController < ApplicationController
       raw_results["hits"]["hits"].each do |hit|
         @items << hit["_source"]["descricao_detalhada_produto"]
       end
-      
+
       render "items/index"
     else 
       #handle the case where it fails....
