ef6ea200b9a2b5e278eea5f7dd95144e28e51b01
Alvaro <alvaroinckot@gmail.com>
2015-09-01 15:57:50 -0300

    Replace routes and create views to search

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,6 +1,25 @@
 class SearchesController < ApplicationController
-  def new
+  
+  def show
+    client = Elasticsearch::Client.new host: 'http://104.197.50.109:9400';
+
+    search = Search.new(:query => params[:q])
+    search.user = current_user
+
+    if search.save
+      raw_results = client.search(q: search.query)
+
+      @items = []
+      raw_results["hits"]["hits"].each do |hit|
+        @items << hit["_source"]["descricao_detalhada_produto"]
+      end
+
+      render "items/index"
+    else 
+      #handle the case where it fails....
+    end
   end
+
   def create
     #TODO: Associate Searches with a user
     client = Elasticsearch::Client.new host: 'http://104.197.50.109:9400';
--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,10 @@ Rails.application.routes.draw do
 
   devise_for :users
 
-  resources :searches
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
+  resource :search
 
+  get 'search', to: 'searches#index'
+  
   # You can have the root of your site routed with "root"
    root 'welcome#index'
 
