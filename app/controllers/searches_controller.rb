e0c2985fc3bd3d2bdf93074413585b49fe744fba
Alvaro <alvaroinckot@gmail.com>
2015-09-03 11:21:54 -0300

    improve details on code

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -5,31 +5,29 @@ class SearchesController < ApplicationController
   end
 
   def create
-    
+
     @search = Search.new(:query => params[:query])
 
     if @search.save
-      
+
       raw_results = search_client.search(q: @search.query)
 
       @products = []
 
       raw_results["hits"]["hits"].each do |hit|
-        @products << hit["_source"]#["descricao_detalhada_produto"]
+        @products << hit["_source"]
       end
 
-    else 
+    else
       #handle the case where it fails....
     end
-
-    # expires_in 3.minutes, :public => true
 
   end
 
   private
 
-    def search_client
-      Elasticsearch::Client.new host: 'http://104.197.50.109:9400'
-    end
+  def search_client
+    Elasticsearch::Client.new host: 'http://104.197.50.109:9400'
+  end
 
 end
