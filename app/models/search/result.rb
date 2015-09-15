0273a1d6545f327df5080d81e523c2bcdc188170
Alvaro <alvaroinckot@gmail.com>
2015-09-15 15:49:26 -0300

    Improve code quality at search results

--- app/models/search/result.rb
+++ app/models/search/result.rb
@@ -6,22 +6,49 @@ class Search::Result
   attr_accessor :products, :cif_total, :filters, :range_filters, :hits, :pagination
 
   def initialize raw_results, filters = [], range_filters = [], pagination = nil
+    parse_results(raw_results)
+    build_pagination(pagination)
+    update_selection_filters(raw_results, filters)
+    update_range_filters(raw_results, range_filters)
+  end
 
+
+  private
+
+  def parse_results raw_results
+    self.filters = []
     self.range_filters = Hash.new
     self.hits = raw_results["hits"]["total"]
     self.products = raw_results["hits"]["hits"].map { |hit| hit["_source"]  }
     self.cif_total = raw_results["aggregations"]["cif"]["value"]
-    
+  end
+
+  def build_pagination pagination
     self.pagination = pagination
     self.pagination.total_pages = (self.hits / self.pagination.count).to_i
+  end
 
-    #if don't have any filters in request, get it by search raw_results
-    if filters == nil
-      create_selection_filters raw_results
-    else
-      update_selection_filters raw_results, filters
-    end
+  def update_selection_filters results, filters
 
+    filters ||=  []
+    skip = Search::SelectionFilterType::Ncm
+
+    Search::SelectionFilterType.constants.each do |type|
+      
+      if type == skip && filters.count > 0 
+        self.filters += filters.select { |f| f.type == skip }
+        next
+      end
+
+      self.filters += results["aggregations"][type.to_s]["buckets"].map { |e| 
+        Search::SelectionFilter.new e["key"], e["doc_count"].to_i,  
+        filters.any? { |f| f.value == e["key"] && f.type.intern == type && f.selected }, type 
+      }
+
+    end   
+  end
+
+  def update_range_filters results, range_filters
 
     if range_filters == nil
       Search::RangeFilterType.constants.each do |type|
@@ -31,33 +58,6 @@ class Search::Result
       range_filters.each do |range_filter|
         self.range_filters[range_filter.type] = range_filter
       end
-    end
-
-  end
-
-
-  private
-
-  def create_selection_filters results
-
-    self.filters = []
-
-    Search::SelectionFilterType.constants.each do |type|
-      self.filters += results["aggregations"][type.to_s]["buckets"].map { |e| Search::SelectionFilter.new e["key"], e["doc_count"].to_i, false, type }
-    end
-
-  end
-
-  def update_selection_filters results, filters
-
-    #set all filters
-    self.filters = filters
-
-    #update contry filters
-    Search::SelectionFilterType.constants.select { |t| t != Search::SelectionFilterType::Ncm }.each do |type|
-      selected_countries = self.filters.select { |e| e.type.intern == type.intern && e.selected }
-      self.filters.delete_if { |e| e.type.intern == type.intern }
-      self.filters += results["aggregations"][type.to_s]["buckets"].map { |e| Search::SelectionFilter.new e["key"], e["doc_count"].to_i, selected_countries.any? { |f| f.value == e["key"] }, type.to_s }
     end
 
   end
