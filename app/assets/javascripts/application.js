4645c505a2bb291d141e1427d2bb3f78dc530059
Alvaro <alvaroinckot@gmail.com>
2015-09-21 14:50:48 -0300

    Update assets version

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,6 +20,7 @@
 //= require jquery-ui/core
 //= require jquery-ui/widget
 
+
 $.urlParam = function(name){
     var results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(window.location.href);
     if (results==null){
