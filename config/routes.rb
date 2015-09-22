261136442aa64417aa428ee7107449042014227c
Alvaro <alvaroinckot@gmail.com>
2015-09-22 11:45:22 -0300

    fix route bugs

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   get 'welcome/index'
 
-  devise_for :users
-
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
 
   resource :search
