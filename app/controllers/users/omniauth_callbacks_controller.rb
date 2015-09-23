7aadc3b95612082a9fb770d691b5a076241d4b4d
Alvaro <alvaroinckot@gmail.com>
2015-09-23 17:41:19 -0300

    remove facebook skip

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -3,7 +3,6 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     # You need to implement the method below in your model (e.g. app/models/user.rb)
     @user = User.from_omniauth(request.env["omniauth.auth"])
     if @user.persisted?
-      @user.skip_confirmation! 
       sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
       set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
     else
@@ -15,7 +14,6 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     auth = env["omniauth.auth"]
     @user = User.from_omniauth(request.env["omniauth.auth"])
     if @user.persisted?
-      @user.skip_confirmation! 
       sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
       set_flash_message(:notice, :success, :kind => "Linkedin") if is_navigational_format?
     else
