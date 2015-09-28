efeef77037daa293ab44f35f1e54c35b3786eef8
Alvaro Cantador <alvaroinckot@gmail.com>
2015-09-28 17:20:06 -0300

    fix ux uikit mobile

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -18,7 +18,9 @@ class SearchesController < ApplicationController
     if @search.save
       query_builder = Search::QueryBuilder.new
       query = query_builder.build(@search.query, selection_filters, range_filters, pagination, date_range_filters, sort_by) 
+      #@query = range_filters
       raw_results = client.search(query)
+      #@results = raw_results
       @result = Search::Result.new(raw_results, selection_filters, range_filters, query_builder.pagination, date_range_filters,sort_by)
     else
       # todo handle the case where it fails....
