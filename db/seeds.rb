a2033d1f9068dde0a3b93ca64152aefd7e32e7be
Alvaro <alvaroinckot@gmail.com>
2015-09-29 13:31:28 -0300

    seed only an admin user

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,32 +19,12 @@ end
    email:    'admin@pinho.com',
    password: 'Pinho@15',
  )
+
+ admin.role = Role.find_by({name: 'admin'})
  
  admin.skip_confirmation!
  admin.save!
  
- # Create a moderator
- moderator = User.new(
-   name:     'Moderator User',
-   email:    'carlos@comex.guru',
-   password: 'helloworld',
- )
- moderator.skip_confirmation!
- moderator.save!
- 
-
- # Create a member
- member = User.new(
-   name:     'Member User',
-   email:    'member@example.com',
-   password: 'helloworld'
- )
-
-
- member.skip_confirmation!
- member.save!
-
-
 puts "Seed finished"
 
 puts "#{User.count} users created"
