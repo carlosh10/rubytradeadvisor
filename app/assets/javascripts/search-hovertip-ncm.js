25c36d6dd1f338225b769405bf53734890d01e73
Alvaro <alvaroinckot@gmail.com>
2015-09-29 11:10:48 -0300

    support hovertip ncm to uikit

--- app/assets/javascripts/search-hovertip-ncm.js
+++ app/assets/javascripts/search-hovertip-ncm.js
@@ -1,14 +1,14 @@
 $(document).ready(function(){
 	$(".form-ncm").each(function(){
 		var $this = $(this);	
-			/*		
+			
 			$.get($(this).attr('action'), $(this).serialize(), function(data){
 				$("span[data-ncm=" + $this.find("[name=ncm]").first().val() + "]").attr({
 						"data-uk-tooltip": "{pos:'top-left'}",
 						"title": data["key"]
 					});
 			});
-			*/
+			
 		});
 
 });
