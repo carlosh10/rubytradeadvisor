7e6960cf8cfd91d21f4719180f15143e8a1994d6
Alvaro Cantador <alvaroinckot@gmail.com>
2015-12-03 15:52:26 -0200

    uodate user

--- app/models/user.rb
+++ app/models/user.rb
@@ -35,17 +35,11 @@ class User < ActiveRecord::Base
   end
 
   def self.find_for_oauth(auth, signed_in_resource = nil)
-
+    
     identity = Identity.find_for_oauth(auth)
-
-    # If a signed_in_resource is provided it always overrides the existing user
-    # to prevent the identity being locked with accidentally created accounts.
     user = signed_in_resource ? signed_in_resource : identity.user
 
     if user.nil?
-      # Get the existing user by email if the provider gives us a verified email.
-      # If no verified email was provided we assign a temporary email and ask the
-      # user to verify it on the next step via UsersController.finish_signup
       # email_is_verified = auth.info.email && (auth.info.verified || auth.info.verified_email)
       email = auth.info.email # if email_is_verified
       user = User.where(:email => email).first if email
@@ -61,7 +55,6 @@ class User < ActiveRecord::Base
       end
     end
 
-    # Associate the identity with the user if needed
     if identity.user != user
       identity.user = user
       identity.save!
